6a2167c7fcd954a117e9d9eaf40fb469581ecfba
lozukka <lotsulotsu@gmail.com>
2025-10-02 13:03:42 +0300

    Update script.js
    render the first icon

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const newerBtn = document.getElementById("newer");
 const nextBtn = document.getElementById("next");
 const resetBtn = document.getElementById("reset");
 const scoreDisplay = document.getElementById("score");
+const firstIconDisplay = document.getElementById("first-icon");
 const secondIconDisplay = document.getElementById("second-icon");
 let score = 0;
 
@@ -18,11 +19,8 @@ nextBtn.addEventListener("click", (event) => {
 
 async function getIcons() {
   const icon = await fetchIcons();
-  //icons.forEach((icon) => {
-  const { image, name, date } = icon;
-  console.log(icon);
-  renderIcons(image, name, date);
-  //});
+  renderFirstIcon(icon[0]);
+  renderSecondIcon(icon[1]);
 }
 
 async function fetchIcons() {
@@ -31,17 +29,32 @@ async function fetchIcons() {
     if (!response.ok) throw new Error("Failed to load icons");
 
     const data = await response.json();
+
     const firstIndex = Math.floor(Math.random() * data.length);
-    const secondIndex = Math.floor(Math.random() * 3);
+    let secondIndex = Math.floor(Math.random() * data.length);
+    while (secondIndex === firstIndex) {
+      secondIndex = Math.floor(Math.random() * data.length);
+    }
     console.log(firstIndex, secondIndex);
     console.log(data[firstIndex], data[secondIndex]);
-    return data[firstIndex], data[secondIndex];
+    return [data[firstIndex], data[secondIndex]];
   } catch (error) {
     console.log(error);
   }
 }
 
-function renderIcons(image, name, date) {
+function renderFirstIcon(icon) {
+  const { image, name, date } = icon;
+  console.log(icon);
+  console.log(image, name, date);
+  firstIconDisplay.innerHTML = `<span id=icon2>
+<img src="${image}"/></span>
+<p>${name}</p>
+<h2>than this?</h2>`;
+}
+function renderSecondIcon(icon) {
+  const { image, name, date } = icon;
+  console.log(icon);
   console.log(image, name, date);
   secondIconDisplay.innerHTML = `<span id=icon2>
 <img src="${image}"/></span>
