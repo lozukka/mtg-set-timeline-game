a5acf17c0279f4f360d374a205176d3d0c97e68b
lozukka <lotsulotsu@gmail.com>
2025-10-02 11:24:50 +0300

    setting up the project
    created files, made html structure and linking the files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mtg-set-timeline-game
+
+json-file structure:
+
+```
+{
+    "icon": "",
+    "name": "",
+    "date": "2020-01-24"
+  }
+```
---
+++ script.js
@@ -0,0 +1,9 @@
+const olderBtn = document.getElementById("older");
+const newerBtn = document.getElementById("newer");
+
+olderBtn.addEventListener("click", (event) => {
+  console.log("older!");
+});
+newerBtn.addEventListener("click", (event) => {
+  console.log("newer!");
+});
